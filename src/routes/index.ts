4f55672f5fbee50f7cf7dad22332a2f27a8ddfbe
Samuel João Barbosa Pinto <samuel_jbp@yahoo.com.br>
2023-05-15 11:29:00 -0300

    final project 1.0

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import * as SearchController from "../controllers/searchController";
 const router = Router();
 
 router.get("/", PageController.home);
+router.get("/home", PageController.home);
 router.get("/dogs", PageController.dogs);
 router.get("/cats", PageController.cats);
 router.get("/fishes", PageController.fishes);
